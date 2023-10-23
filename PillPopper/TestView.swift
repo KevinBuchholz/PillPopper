ac500dcc6d3e45f57ff7115db9668e284a3f6110
Kevin Buchholz <buchholz.kevin@gmail.com>
2023-10-23 11:39:19 -0400

    Added a testview to test the lazy h stack and scrolling

---
+++ PillPopper/TestView.swift
@@ -0,0 +1,18 @@
+//
+//  TestView.swift
+//  PillPopper
+//
+//  Created by Kevin Buchholz on 10/23/23.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestView()
+}
