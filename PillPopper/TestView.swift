b650d05c756ab45d56cdbe6389115393e4dbd2a7
Kevin Buchholz <buchholz.kevin@gmail.com>
2023-10-24 16:37:40 -0400

    Added SingleDayRectangle and scrollview animations/positioning.

--- PillPopper/TestView.swift
+++ PillPopper/TestView.swift
@@ -7,52 +7,60 @@
 
 import SwiftUI
 
-
 struct TestView: View {
+    
+    @Environment (\.verticalSizeClass) var verticalSizeClass
+    
+    var days : [String] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    
     var body: some View {
         VStack{
-            Spacer()
-            Spacer()
-            Spacer()
-            Spacer()
-            Spacer()
+            //            Spacer()
+            //            Spacer()
+            //            Spacer()
+            //            Spacer()
+            //            Spacer()
             
             ScrollView(.horizontal) {
                 
-                LazyHStack(alignment: .top, spacing: 15, pinnedViews: /*@START_MENU_TOKEN@*/[]/*@END_MENU_TOKEN@*/, content: {
+                LazyHStack(alignment: .center, spacing: 15, pinnedViews: /*@START_MENU_TOKEN@*/[]/*@END_MENU_TOKEN@*/, content: {
                     
                     ForEach(1...10, id: \.self) { count in
-                        RoundedRectangle(cornerRadius: 15)
-                            .aspectRatio(1, contentMode: .fit)
-//                            .frame(maxWidth: .infinity, maxHeight: 250, alignment: .trailing)
-                            .foregroundColor(.pink)
-                            .opacity(0.1)
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 15)
-                                    .stroke(.black, lineWidth: 1)
-                                    .opacity(0.2)
-                            )
+                        SingleDayRectangle()
+                    }
+
+                    .containerRelativeFrame(/*@START_MENU_TOKEN@*/.horizontal/*@END_MENU_TOKEN@*/, count: verticalSizeClass == .regular ? 1 : 3, spacing: 15)
+                    .scrollTransition { content, phase in content
+                            .opacity(phase .isIdentity ? 1.0 : 0.3)
+//                            .scaleEffect(x: phase .isIdentity ? 1.0 : 0.3, y: phase .isIdentity ? 1.0 : 0.8)
+//                            .offset(y: phase .isIdentity ? 0 : 50)
                     }
                 })
+                .scrollTargetLayout()
             }
-            .flipsForRightToLeftLayoutDirection(true)
+            .contentMargins(30, for: .scrollContent)
+            .scrollTargetBehavior(.viewAligned)
+//            .flipsForRightToLeftLayoutDirection(true)
             .environment(\.layoutDirection, .rightToLeft)
-            //        .defaultScrollAnchor(.center)
             .scrollIndicators(.hidden)
+           
             
-            Text("Take a pill")
-                .font(.largeTitle)
-                .padding()
+            
+//            Text("Take a pill")
+//                .font(.largeTitle)
+//                .padding()
             
             RoundedRectangle(cornerRadius: 15)
                 .padding()
                 .frame(maxWidth: .infinity, maxHeight: 200)
             
-            Spacer()
+                        Spacer()
             
         }
     }
 }
+
+
 
 #Preview {
     TestView()
